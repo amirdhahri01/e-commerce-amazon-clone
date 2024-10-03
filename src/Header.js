994e36efd47c3050426617b72138ba8d106576ab
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-03 19:59:48 +0100

    The header component styling is done

--- src/Header.js
+++ src/Header.js
@@ -6,26 +6,26 @@ import SearchIcon from "@mui/icons-material/Search";
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__logo">
-        <StorefrontIcon className="header__logoImage" fontSize="large" />
-        <h2 className="header__logoTitle">eShop</h2>
+      <div className="header-logo">
+        <StorefrontIcon className="header-logoImage" fontSize="large" />
+        <h2 className="header-logoTitle">eShop</h2>
       </div>
-      <div className="header__search">
+      <div className="header-search">
         <input type="text" className="header_searchInput" />
-        <SearchIcon className="header__searchIcon" />
+        <SearchIcon className="header-searchIcon" />
       </div>
-      <div className="header__nav">
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Hello Guest</span>
-          <span className="nav__itemLineTwo">Sign In</span>
+      <div className="header-nav">
+        <div className="nav-item">
+          <span className="nav-itemLineOne">Hello Guest</span>
+          <span className="nav-itemLineTwo">Sign In</span>
         </div>
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Your</span>
-          <span className="nav__itemLineTwo">Shop</span>
+        <div className="nav-item">
+          <span className="nav-itemLineOne">Your</span>
+          <span className="nav-itemLineTwo">Shop</span>
         </div>
-        <div className="nav__item">
-          <ShoppingBasketIcon />
-          <span className="nav__itemLineTwo">0</span>
+        <div className="nav-item">
+          <ShoppingBasketIcon className="nav-itemBasket" />
+          <span className="nav-itemLineTwo nav-basketCount">0</span>
         </div>
       </div>
     </div>
