6b1d6d349686803dda65764603375d62831f9cea
Amir Dhahri <amirdhahri01@gmail.com>
2024-10-06 20:31:27 +0100

    Add to basket function is done using Context API

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -2,16 +2,18 @@ import { createContext, useContext, useReducer } from "react";
 
 //Prepare the data layer
 
-export const Statecontext = createContext();
+export const StateContext = createContext();
 
 //Wrap our app and provide the data layer
 
-export const StateProvider = ({ reducer, initialState, children }) => {
-  <Statecontext.Provider value={useReducer(reducer, initialState)}>
-    {children}
-  </Statecontext.Provider>;
+export const StateProvider = ({ initialState, reducer, children }) => {
+  return (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+      {children}
+    </StateContext.Provider>
+  );
 };
 
 //Pull information from the data layer
 
-export const useStateValue = () => useContext(Statecontext);
+export const useStateValue = () => useContext(StateContext);
